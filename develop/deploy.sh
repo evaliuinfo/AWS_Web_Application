6e5d807696d8170e90ab9d9f694aa353557b7673
Eva Liu <evaliuinfo@gmail.com>
2021-03-28 16:53:16 -0400

    Add files via upload

--- develop/deploy.sh
+++ develop/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd ../cloudformation
+cd ../
 echo -e "** Upload Deployment files to S3 **"
 
 S3DeploymentBucket='aws-configs-bucket-uw2'
 S3BucketPrefix='Deployment/App/WebApp'
-aws s3 cp --recursive . s3://${S3DeploymentBucket}/${S3BucketPrefix}/cloudformation 
+aws s3 cp --recursive cloudformation/* s3://${S3DeploymentBucket}/${S3BucketPrefix}/cloudformation 
+aws s3 cp --recursive source/* s3://${S3DeploymentBucket}/${S3BucketPrefix}/source
 
+cd cloudformation
 echo -e "** Create AWS Root Stack **"
 aws cloudformation create-stack --stack-name $1 --template-body file://root.yml --region=us-west-2 --capabilities CAPABILITY_NAMED_IAM
